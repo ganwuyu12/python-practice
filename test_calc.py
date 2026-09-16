05949295d3d193c340c07aea57f89f8a6d10badb
ganwuyu12 <GitHub>
2026-09-16 20:04:42 +0800

    阶段0：添加 calc_cost 的 pytest 测试

---
+++ test_calc.py
@@ -0,0 +1,8 @@
+from chat import calc_cost
+
+def test_calc_cost_zero():
+    assert calc_cost(0, 0) == 0
+
+def test_calc_cost_positive():
+    expected = 5 * 0.02/1e6 + 11 * 4/1e6
+    assert calc_cost(5, 11) == expected
